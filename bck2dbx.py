9ed829b6ed07a54769a00c22907de605c1bfbf44
albert-ribes <albert.ribes@gmail.com>
2017-03-31 20:03:49 +0200

    Update bck2dbx.py

--- bck2dbx.py
+++ bck2dbx.py
@@ -8,53 +8,100 @@ import subprocess
 import sys
 import gzip
 
-# DROPBOX PERSONAL DATA - TO BE FILLED IN
+# DROPBOX PERSONAL DATA, TO BE FILLED-IN
 db_access_token='XXXXXXXX'
 db_path='XXXXXXXX'
 
-# PROGRAM ARGUMENTS
+#PROGRAM ARGUMENTS
 if len(sys.argv) > 1:
 	folder=str(sys.argv[1])
+	if (os.path.exists(folder)==False):
+		print("[ERROR] The folder path provided doesn't exist.")
+		sys.exit()
 else:
-	print('Usage: ' + sys.argv[0] + ' [FOLDER_PATH]')
+	print('Usage: ' + sys.argv[0] + ' [SOURCE_FOLDER_PATH]')
 	sys.exit()
 
-# VARIABLES
+#TIME
 now=time.strftime("%Y%m%d_%H%M%S", time.localtime())
+
+#VARIABLES
 FNULL = open(os.devnull, 'w')
-path=os.path.dirname(os.path.realpath(__file__))
-#path='/home/'
+p1 = subprocess.Popen(["pwd"], stdout=subprocess.PIPE,stderr=subprocess.PIPE)
+out, err = p1.communicate()
+path=str(out)[2:-3]+'/'
 filename='bckp-'+now+'.tar.gz'
 
-print('[INFO] This program will backup the folder `' + folder + '` to `' + db_path + filename + '`...')
+print('\n[INFO] Starting program. It will backup `' + folder + '` to `' + db_path + filename)
+print('[INFO] Actual path: `'+ path+'`')
 print("[INFO] Authenticating at Dropbox... ", end="")
+sys.stdout.flush()
 
+#DROPBOX AUTHENTICATION
 try:
-  dbx = dropbox.Dropbox(db_access_token)
-  account = dbx.users_get_current_account()
-	print("OK")
-	#print('	- Account_id: ' + account.account_id)
-	#print('	- Account_name: ' + account.name.given_name)
-	#print('	- Account_e-mail: ' + account.email)
+	dbx = dropbox.Dropbox(db_access_token)
+	account = dbx.users_get_current_account()
+	print("[OK]")
+	#print('	- Modified: `' + dbx.files_get_metadata('/Backup/').client_modified + '`')
+	print('	- Account_id: `' + account.account_id + '`')
+	print('	- Account_name: `' + account.name.given_name+ '`')
+	print('	- Account_e-mail: `' + account.email+ '`')
+	#print('	- Account_type: `' + account.account_type+ '`')
+	#sys.stdout.flush()
 except:
-	print("Authentication NOT successful!")
+	print("[KO]")
+	print("[ERROR] Authentication NOT successful!")
 	sys.exit()
 
+#FILE COMPRESSION
+print("[INFO] Compressing folders and files... ", end="")
+sys.stdout.flush()
 try:
-	command='tar -zcvf ' + path + filename + ' ' + folder +' -R'
-	print("[INFO] Compressing folders and files... ", end="")
-	subprocess.call(command.split(), shell=False, stdout=FNULL, stderr=subprocess.STDOUT)
-	command='chmod 777 ' + path + filename
-	subprocess.call(command.split(), shell=False, stdout=FNULL, stderr=subprocess.STDOUT)
-	print("OK")
+	#command='tar -zcvf ' + path + filename + ' ' + folder +' -R'
+	p1 = subprocess.Popen(["tar","-zcvf",path+filename,folder,"-R"], stdout=subprocess.PIPE,stderr=subprocess.PIPE)
+	out, err = p1.communicate()
+	#print(out)
+	#print(err)
+	p1.wait();
+	#command='chmod 777 ' + path + filename
+	p2 = subprocess.Popen(["chmod","777",path+filename], stdout=subprocess.PIPE,stderr=subprocess.PIPE)
+	#print(out)
+	#print(err)
+	p2.wait();
+	print("[OK]")
+	print('	- Teomporary file: `' + path+filename+ '`')
 except:
+	print("[KO]")
 	print('[ERROR] Something went wrong during compression of '+ path + filename)
 	sys.exit()
 
+#DROPBOX UPLOAD
 print("[INFO] Uploading to Dropbox... ", end="")
+sys.stdout.flush()
 try:
 	f = gzip.open(path + filename, mode='r')
-	fileupload = dbx.files_upload(f,'/Backup/'+filename)
-	print("OK")
+	fileupload = dbx.files_upload(f,'/Backup/'+filename) #Method used to upload files smaller than 150MB
+	print("[OK]")
 except:
-	print("[ERROR] Ouch! Something went wrong!")
+	print("[KO]")
+	print("\n[ERROR] Ouch! Something went wrong!")
+	sys.exit()
+
+print('	- Filename: `' + db_path+filename + '`')
+print('	- Modified: `' + str(dbx.files_get_metadata('/Backup/'+filename).client_modified) + '`')
+
+#DELETING LOCAL FILE
+print("[INFO] Deleting local file `" +path+filename + '`... ', end="")
+sys.stdout.flush()
+try:
+
+	p1 = subprocess.Popen(["rm","-f",path+filename], stdout=subprocess.PIPE,stderr=subprocess.PIPE)
+	out, err = p1.communicate()
+	#print(out)
+	#print(err)
+	p1.wait();
+	print("[OK]")
+except:
+	print("[KO]")
+	print('[ERROR] Something went wrong during compression of '+ path + filename)
+	sys.exit()
