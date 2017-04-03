0ccf50dc8cdd50f0246e9ddd0153b84b9e295a26
Albert Ribes <albert.ribes@gmail.com>
2017-04-03 11:09:59 +0200

    Canvi remot

--- bck2dbx.py
+++ bck2dbx.py
@@ -1,16 +1,22 @@
 #!/usr/bin/python3
 
+#imports
 import dropbox
+import dbx_data
 import sys
 import time
 import os
 import subprocess
 import sys
 import gzip
+from blessings import Terminal
+term = Terminal()
 
+"""
 # DROPBOX PERSONAL DATA, TO BE FILLED-IN
-db_access_token='XXXXXXXX'
-db_path='XXXXXXXX'
+dbx_access_token='XXXXXXXX'
+dbx_path='XXXXXXXX'
+"""
 
 #PROGRAM ARGUMENTS
 if len(sys.argv) > 1:
@@ -32,16 +38,16 @@ out, err = p1.communicate()
 path=str(out)[2:-3]+'/'
 filename='bckp-'+now+'.tar.gz'
 
-print('\n[INFO] Starting program. It will backup `' + folder + '` to `' + db_path + filename)
+print('\n[INFO] Starting program. It will backup `' + folder + '` to `' + dbx_data.dbx_path + filename)
 print('[INFO] Actual path: `'+ path+'`')
 print("[INFO] Authenticating at Dropbox... ", end="")
 sys.stdout.flush()
 
 #DROPBOX AUTHENTICATION
 try:
-	dbx = dropbox.Dropbox(db_access_token)
+	dbx = dropbox.Dropbox(dbx_data.dbx_access_token)
 	account = dbx.users_get_current_account()
-	print("[OK]")
+	print(term.black_on_green("						[OK]"))
 	#print('	- Modified: `' + dbx.files_get_metadata('/Backup/').client_modified + '`')
 	print('	- Account_id: `' + account.account_id + '`')
 	print('	- Account_name: `' + account.name.given_name+ '`')
@@ -49,7 +55,7 @@ try:
 	#print('	- Account_type: `' + account.account_type+ '`')
 	#sys.stdout.flush()
 except:
-	print("[KO]")
+	print(term.black_on_red("		[KO]"))
 	print("[ERROR] Authentication NOT successful!")
 	sys.exit()
 
@@ -58,20 +64,20 @@ print("[INFO] Compressing folders and files... ", end="")
 sys.stdout.flush()
 try:
 	#command='tar -zcvf ' + path + filename + ' ' + folder +' -R'
-	p1 = subprocess.Popen(["tar","-zcvf",path+filename,folder,"-R"], stdout=subprocess.PIPE,stderr=subprocess.PIPE)
+	p1 = subprocess.Popen(["tar","-zcvf","/tmp/"+filename,folder,"-R"], stdout=subprocess.PIPE,stderr=subprocess.PIPE)
 	out, err = p1.communicate()
 	#print(out)
 	#print(err)
 	p1.wait();
 	#command='chmod 777 ' + path + filename
-	p2 = subprocess.Popen(["chmod","777",path+filename], stdout=subprocess.PIPE,stderr=subprocess.PIPE)
+	p2 = subprocess.Popen(["chmod","777","/tmp/"+filename], stdout=subprocess.PIPE,stderr=subprocess.PIPE)
 	#print(out)
 	#print(err)
 	p2.wait();
-	print("[OK]")
-	print('	- Teomporary file: `' + path+filename+ '`')
+	print(term.black_on_green("					[OK]"))
+	print('	- Teomporary file: `' + "/tmp/"+filename+ '`')
 except:
-	print("[KO]")
+	print("		[KO]")
 	print('[ERROR] Something went wrong during compression of '+ path + filename)
 	sys.exit()
 
@@ -79,29 +85,29 @@ except:
 print("[INFO] Uploading to Dropbox... ", end="")
 sys.stdout.flush()
 try:
-	f = gzip.open(path + filename, mode='r')
+	f = gzip.open("/tmp/"+filename, mode='r')
 	fileupload = dbx.files_upload(f,'/Backup/'+filename) #Method used to upload files smaller than 150MB
-	print("[OK]")
+	print(term.black_on_green("							[OK]"))
 except:
-	print("[KO]")
+	print(term.black_on_red("			[KO]"))
 	print("\n[ERROR] Ouch! Something went wrong!")
 	sys.exit()
 
-print('	- Filename: `' + db_path+filename + '`')
+print('	- Filename: `' + dbx_data.dbx_path+filename + '`')
 print('	- Modified: `' + str(dbx.files_get_metadata('/Backup/'+filename).client_modified) + '`')
 
 #DELETING LOCAL FILE
-print("[INFO] Deleting local file `" +path+filename + '`... ', end="")
+print("[INFO] Deleting local file `" +"/tmp/"+filename + '`... ', end="")
 sys.stdout.flush()
 try:
 
-	p1 = subprocess.Popen(["rm","-f",path+filename], stdout=subprocess.PIPE,stderr=subprocess.PIPE)
+	p1 = subprocess.Popen(["rm","-f","/tmp/"+filename], stdout=subprocess.PIPE,stderr=subprocess.PIPE)
 	out, err = p1.communicate()
 	#print(out)
 	#print(err)
 	p1.wait();
-	print("[OK]")
+	print(term.black_on_green("		[OK]"))
 except:
-	print("[KO]")
+	print(term.black_on_red("	[KO]"))
 	print('[ERROR] Something went wrong during compression of '+ path + filename)
 	sys.exit()
